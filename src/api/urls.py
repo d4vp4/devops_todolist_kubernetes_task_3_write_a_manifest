19a3bb79ec5b7dcaeadc4d3dfb5751f2ed47b6b7
Danylo Povar <danila74povar@gmail.com>
2026-02-03 13:47:50 +0200

    task_3.3

--- src/api/urls.py
+++ src/api/urls.py
@@ -11,10 +11,10 @@ router.register(r"todos", views.TodoViewSet)
 
 app_name = "api"
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
 ]
 
-urlpatterns = [
+urlpatterns += [
     path('liveness/', views.liveness),
     path('readiness/', views.readiness),
 ]
